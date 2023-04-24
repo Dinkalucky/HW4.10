32f18c0c56a0c2d4a6d63ea88df2a86e090b39d1
Dina Nevhod <Dina_Nevhod@epam.com>
2023-04-24 12:40:03 +0200

    Add project files.

---
+++ Task10.2/Base.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10._2
+{
+    public class Base
+    {
+        public void BaseMethod()
+        {
+            MethodFirst();
+            MethodSecond();
+        }
+
+        protected virtual void MethodFirst()
+        {
+            Console.WriteLine("BaseMethodFirst");
+        }
+        protected virtual void MethodSecond()
+        {
+            Console.WriteLine("BaseMethodSecond");
+        }
+    }
+}
---
+++ Task10.2/Derived.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10._2
+{
+    class Derived : Base
+    {
+        protected new void MethodFirst()
+        {
+            Console.WriteLine("DerivedMethodFirst");
+        }
+        protected override void MethodSecond()
+        {
+            Console.WriteLine("DerivedMethodSecond");
+        }
+    }
+}
